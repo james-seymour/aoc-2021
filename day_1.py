e850b419e972d0ce7893e17bc8d4b2d33a98f7fc
james-seymour <jsjamesemail@gmail.com>
2021-12-01 15:08:32 +1000

    Day 1 finished

--- day_1.py
+++ day_1.py
@@ -1,6 +1,23 @@
 from util import *
 
+data = parse_to_list(read_input(1))
 
-with open("day_1_input.txt") as f:
-    data = parse_to_tuple(f.readlines())
+# Part 1
+increases = 0
+for depth, next_depth in zip(data, data[1:]):
+    if next_depth > depth:
+        increases += 1
 
+print(increases)
+
+# Part 2
+sliding_window = []
+for first_depth, second_depth, third_depth in zip(data, data[1:], data[2:]):
+    sliding_window.append(first_depth + second_depth + third_depth)
+
+increases = 0
+for depth, next_depth in zip(sliding_window, sliding_window[1:]):
+    if next_depth > depth:
+        increases += 1
+
+print(increases)
--- util.py
+++ util.py
@@ -1,5 +1,7 @@
 
-
+def read_input(day: int):
+    with open(f"day_{day}_input.txt") as f:
+        return f.readlines()
 
 def parse_to_tuple(lines):
     return tuple(map(int, (lines)))
