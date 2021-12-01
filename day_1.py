296a5ebf81eddb864f88627c41b62a435919ca3f
james-seymour <jsjamesemail@gmail.com>
2021-12-01 14:40:21 +1000

    Initial Commit

---
+++ day_1.py
@@ -0,0 +1,6 @@
+from util import *
+
+
+with open("day_1_input.txt") as f:
+    data = parse_to_tuple(f.readlines())
+
---
+++ util.py
@@ -0,0 +1,8 @@
+
+
+
+def parse_to_tuple(lines):
+    return tuple(map(int, (lines)))
+
+def parse_to_list(lines):
+    return list(map(int, (lines)))
