0a9a7c4b29d3ec8d640d1440f21a63d850390fc8
james-seymour <jsjamesemail@gmail.com>
2021-12-09 17:25:00 +1000

    Updated Day 9

--- day_9_a.py
+++ day_9_a.py
@@ -5,20 +5,19 @@ data = read_aoc_input(9)
 
 data = [[int(char) for char in line] for line in data ]
 
+
 t = 0
 for r_i, r in enumerate(data):
     for c_i, c in enumerate(r):
         ns = []
-        adj = list(neighbours((r_i, c_i)))
 
-        for n_r, n_c in adj:
-            if n_r in range(len(data)) and n_c in range(len(data[0])):
-                ns.append(data[n_r][n_c])
+        for n_r, n_c in neighbours((r_i, c_i),grid=data):
+            ns.append(data[n_r][n_c])
 
         if False not in [ c < n for n in ns ]:
             t += c + 1
 
-        
+
 print(t) 
 
 
