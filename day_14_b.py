4cd1c7ad96d071734cc19006a3916dd9ea431c59
james-seymour <jsjamesemail@gmail.com>
2021-12-14 16:13:17 +1000

    Tidied Day 14

--- day_14_b.py
+++ day_14_b.py
@@ -1,34 +1,31 @@
 from utils import read_aoc_input
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 data = read_aoc_input(14)
 
 base = data[0].strip()
 rules = dict([line.split(" -> ") for line in data[2:]])
 
-pair_count = defaultdict(int)
-for x, y in zip(base, base[1:]):
-    pair_count[x + y] += 1
+pair_count = Counter([x + y for x, y in zip(base, base[1:])])
 
 for _ in range(40):
     new_pair_count = defaultdict(int)
-    for pair in pair_count.keys():
+    for pair, count in pair_count.items():
         insert = rules[pair]
         first_l, second_l = pair
 
-        new_pair_count[first_l + insert] += pair_count[pair]
-        new_pair_count[insert + second_l] += pair_count[pair]
+        new_pair_count[first_l + insert] += count
+        new_pair_count[insert + second_l] += count
 
     pair_count = new_pair_count
 
-print(pair_count)
 first_count = defaultdict(int)
 second_count = defaultdict(int)
 
-for pair in pair_count.keys():
+for pair, count in pair_count.items():
     first_l, second_l = pair
-    first_count[first_l] += pair_count[pair]
-    second_count[second_l] += pair_count[pair]
+    first_count[first_l] += count
+    second_count[second_l] += count
 
 counts = {char: max(first_count[char], second_count[char]) for char in first_count.keys()}
 
